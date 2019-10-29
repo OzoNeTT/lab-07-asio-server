e5a82b8c61afd6c2052669a52033c9b773ff3ac2
OzoNeTT <gpoff12@mail.ru>
2019-10-29 23:51:49 +0300

    sss

--- sources/Client.cpp
+++
@@ -1,6 +0,0 @@
-#include <boost/asio.hpp>
-#include <boost/asio/deadline_timer.hpp>
-#include <boost/date_time/posix_time/posix_time_duration.hpp>
-#include <boost/bind.hpp>
-#include <boost/optional.hpp>
-#include <boost/log/trivial.hpp>
